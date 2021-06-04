d6066ad6b538a66cdbe23d2172ed1a822a1fd878
Chris <github@mrz.angrydome.com>
2021-06-04 16:37:08 +0100

    add a post merge action

--- scripts/generate_import_data.sh
+++ scripts/generate_import_data.sh
@@ -11,6 +11,7 @@ temp_json_file=$(mktemp)
 snyk-api-import import:data --orgsData=org-data.json --source=github --integrationType=github-enterprise
 
 jq -f org-filter.jq "${SNYK_LOG_PATH}/github-import-targets.json" > "$temp_json_file"
+
 mv "$temp_json_file" "${SNYK_LOG_PATH}/github-import-targets.json"
 
 PATH=$OLD_PATH
