b03009aa473d7f94315866497f4a2c54340d6462
Chris <github@mrz.angrydome.com>
2021-06-04 16:07:10 +0100

    add a post merge action

--- scripts/perform_import.sh
+++ scripts/perform_import.sh
@@ -8,6 +8,10 @@ PATH="${SCRIPTPATH}/bin":$PATH
 
 export SNYK_IMPORT_PATH="${SNYK_LOG_PATH}/github-import-targets.json"
 
+export SNYK_TOKEN=$SNYK_TOKEN
+
+echo "Snyk Secret is: ${SNYK_SECRET}"
+
 snyk-api-import
 
 snyk-api-import list:imported --integrationType=github-enterprise --orgId=39ddc762-b1b9-41ce-ab42-defbe4575bd6
