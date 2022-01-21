e9f3bad3d66d00af358213c0a018559352e06809
Chris Barker <github@mrz.angrydome.com>
2022-01-21 15:20:38 +0000

    Update/groups support (#30)
    * fix
    
    * consolidates groups changes

--- scripts/full-sync.sh
+++ scripts/full-sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GROUP_NAME=$1
+
 docker run --rm -it -e GITHUB_TOKEN -e SNYK_TOKEN -v "${PWD}":/runtime \
 ghcr.io/snyk-playground/snyk-sync:latest --sync targets --save
 
@@ -7,7 +9,7 @@ export SNYK_LOG_PATH="cache/logs"
 
 docker run --rm -it -e SNYK_LOG_PATH -e SNYK_TOKEN -v "${PWD}":/runtime \
 --entrypoint '/usr/local/bin/entrypoint-api-import.sh' \
-ghcr.io/snyk-playground/snyk-sync:latest import --file import-targets.json
+ghcr.io/snyk-playground/snyk-sync:latest import --file targets/$GROUP_NAME.json
 
 docker run --rm -it -e GITHUB_TOKEN -e SNYK_TOKEN -v "${PWD}":/runtime \
 ghcr.io/snyk-playground/snyk-sync:latest --sync tags --update
