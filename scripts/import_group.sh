bc46e02af121ec313b5847364f2b2e06f606e8d6
Chris <github@mrz.angrydome.com>
2022-01-21 15:16:53 +0000

    consolidates groups changes

---
+++ scripts/import_group.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GROUP_NAME=$1
+
+export SNYK_LOG_PATH="cache/logs"
+
+docker run --rm -it -e SNYK_LOG_PATH -e SNYK_TOKEN -v "${PWD}":/runtime \
+--entrypoint '/usr/local/bin/entrypoint-api-import.sh' \
+ghcr.io/snyk-playground/snyk-sync:latest import --file targets/$GROUP_NAME.json
---
+++ scripts/update_tags.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --rm -it -e GITHUB_TOKEN -e SNYK_TOKEN -v "${PWD}":/runtime \
+ghcr.io/snyk-playground/snyk-sync:latest --sync tags --update
